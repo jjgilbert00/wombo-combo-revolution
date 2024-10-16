194140bef60c16eab56b4f0f4854fcf9883bc27a
Joseph Gilbert <joseph.j.gilbert@gmail.com>
2024-10-16 14:52:16 -0400

    Refactored ColumnWidget as ColumnLayout

--- layouts/playalong_layout.py
+++ layouts/playalong_layout.py
@@ -4,7 +4,7 @@ from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color
 
 from controller import ControllerReader
-from widgets import ButtonImage, StickImage, DraggableWidget, DirectionalPromptWidget
+from widgets import ButtonImage, ButtonPromptWidget, StickImage, DraggableWidget, DirectionalPromptWidget
 
 
 
@@ -24,37 +24,20 @@ class JoystickLayout(RelativeLayout):
         self.stick_image.update_state(direction)
 
 
-class ColumnWidget(DraggableWidget):
-    button_image = None
+class ColumnLayout(RelativeLayout):
     border = None
+    button_prompt_widget = None
+
     def __init__(self, **kwargs):
-        super(ColumnWidget, self).__init__(**kwargs)
+        super(ColumnLayout, self).__init__(**kwargs)
         with self.canvas:
             Color(0,0,1,0.25)
             self.border = Rectangle(pos=self.pos, size=self.size)
-            self.button_image = ButtonImage(pos=self.pos, size_hint=(1, 1))
-        self.bind(pos=self.update_canvas)
-        self.bind(size=self.update_canvas)
-        self.update_canvas()
-    
-    def update_state(self, pressed):
-        self.button_image.update_state(pressed)
-        self.canvas.ask_update()
+            self.button_prompt_widget = ButtonPromptWidget(pos_hint={"center_x": 0.5, "center_y": 0.5}, size=self.size)
+            self.add_widget(self.button_prompt_widget)
 
-    def update_canvas(self, *args):
-        # Draw the border of the column
-        Color(0,0,1,0.25)
-
-        self.border.pos = self.pos
-        self.border.size = self.size
-
-        # Draw the descending inputs
-        # print("TODO: Draw the descending inputs")
-
-        # Draw the button silhouette at the bottom of the screen
-        self.button_image.size = (self.width, self.width)
-        self.button_image.pos = self.pos
-
+    def update_state(self, state):
+        self.button_prompt_widget.update_state(state)
 
 class PlayAlongLayout(RelativeLayout):
     def __init__(self, controller_reader: ControllerReader, **kwargs):
@@ -65,7 +48,7 @@ class PlayAlongLayout(RelativeLayout):
         }
         button_names = ["A", "X", "B", "Y", "RT", "RB", "LT", "LB"]
         for i in range(len(button_names)):
-            self.button_displays[button_names[i]] = ColumnWidget(size_hint=(0.1, 1), pos_hint={'x': 0.5 + i * (0.4/8), 'y': 0 if i%2==0 else 0.1})
+            self.button_displays[button_names[i]] = ColumnLayout(size_hint=(0.1, 1), pos_hint={'x': 0.5 + i * (0.4/8), 'y': 0 if i%2==0 else 0.1})
         [self.add_widget(self.button_displays[button]) for button in self.button_displays]
         self.bind(size=self.update_joystick_layout_size)
         self.update_controller_display()
--- widgets.py
+++ widgets.py
@@ -2,46 +2,11 @@
 from kivy.uix.image import Image as CoreImage
 from kivy.properties import BooleanProperty
 from kivy.uix.widget import Widget
-from kivy.graphics import Ellipse, Color
+from kivy.graphics import Ellipse, Rectangle, Color
 
 from images import IMAGE_SOURCE_5, IMAGE_SOURCE_DIRECTION, IMAGE_SOURCE_BUTTON_DOWN, IMAGE_SOURCE_BUTTON_UP
-class StickImage(CoreImage):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.source = IMAGE_SOURCE_5
-    
-    def update_state(self, direction):
-        self.source =  IMAGE_SOURCE_DIRECTION[direction]
-
-class DirectionalPromptWidget(Widget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        with self.canvas:
-            Color(1,1,1,0.25)
-            self.border = Ellipse(pos=self.pos, size=self.size)
-    
-        self.bind(pos=self.update_canvas)
-        self.bind(size=self.update_canvas)
-        self.update_canvas()
-    
-    def update_canvas(self, *args):
-        self.border.pos = self.pos
-        self.border.size = self.size
-    
 
 
-class ButtonImage(CoreImage):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.source = IMAGE_SOURCE_BUTTON_UP
-
-    def update_state(self, pressed):
-        if pressed:
-            self.source = IMAGE_SOURCE_BUTTON_DOWN
-        else:
-            self.source = IMAGE_SOURCE_BUTTON_UP
-
-    
 class DraggableWidget(Widget):
     dragging = BooleanProperty(False)
 
@@ -66,3 +31,75 @@ class DraggableWidget(Widget):
             self.dragging = False
             return True
         return super().on_touch_up(touch)
+
+
+class StickImage(CoreImage):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.source = IMAGE_SOURCE_5
+    
+    def update_state(self, direction):
+        self.source =  IMAGE_SOURCE_DIRECTION[direction]
+
+class DirectionalPromptWidget(Widget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        with self.canvas:
+            Color(1,1,1,0.25)
+            self.border = Ellipse(pos=self.pos, size=self.size)
+    
+        self.bind(pos=self.update_canvas)
+        self.bind(size=self.update_canvas)
+        self.update_canvas()
+    
+    def update_canvas(self, *args):
+        self.border.pos = self.pos
+        self.border.size = self.size
+    
+class ButtonPromptWidget(DraggableWidget):
+    controller_button = None
+    border = None
+    button_pool = []
+    active_buttons = []
+    button_source = None
+    def __init__(self, button_source=IMAGE_SOURCE_BUTTON_UP, **kwargs):
+        super(ButtonPromptWidget, self).__init__(**kwargs)
+        self.button_source = button_source
+        with self.canvas:
+            Color(0,0,1,0.25)
+            self.border = Rectangle(pos=self.pos, size=self.size)
+            self.controller_button = ButtonImage(pos=self.pos, size_hint=(1, 1))
+        self.bind(pos=self.update_canvas)
+        self.bind(size=self.update_canvas)
+        self.update_canvas()
+    
+    def update_state(self, pressed):
+        self.controller_button.update_state(pressed)
+        self.canvas.ask_update()
+
+    def update_canvas(self, *args):
+        # Draw the border of the column
+        Color(0,0,1,0.25)
+
+        self.border.pos = self.pos
+        self.border.size = self.size
+
+        # Draw the descending inputs
+        # print("TODO: Draw the descending inputs")
+
+        # Draw the button silhouette at the bottom of the screen
+        self.controller_button.size = (self.width, self.width)
+        self.controller_button.pos = self.pos
+
+class ButtonImage(CoreImage):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.source = IMAGE_SOURCE_BUTTON_UP
+
+    def update_state(self, pressed):
+        if pressed:
+            self.source = IMAGE_SOURCE_BUTTON_DOWN
+        else:
+            self.source = IMAGE_SOURCE_BUTTON_UP
+
+    
